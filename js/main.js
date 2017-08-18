9f81e160fb4ce7cfaf6fb0716217b5b6ba7699d1
Patil <n0225550@libp45p-35426dd.lm.lmig.com>
2017-08-18 14:09:49 -0400

    created game logic - if conditions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+var cardTwo = cards[3];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//logic to check if two cards are played
+if (cardsInPlay.length === 2) {
+  if ( cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You have found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
