f7cd2d51854ab130e43cbba8df8e121687d5556f
Patil <n0225550@libp45p-35426dd.lm.lmig.com>
2017-08-18 16:24:03 -0400

    updated game logic - unit 9

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-var cardTwo = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//logic to check if two cards are played
-if (cardsInPlay.length === 2) {
-  if ( cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You have found a match!");
-  } else {
-    alert("Sorry, try again.");
+var checkForMatch = function () {
+  if (cardsInPlay.length === 2) {
+    if ( cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You have found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
-}
+};
+
+var flipCard = function (cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User filpped " + cards[cardId]);
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
