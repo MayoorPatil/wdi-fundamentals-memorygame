49be56e3eb75b420c4c8bb4cfd8178b8d0707eec
Patil <n0225550@libp45p-35426dd.lm.lmig.com>
2017-08-20 18:37:52 -0400

    updated game logic - unit 10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -12,8 +33,10 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User filpped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User filpped " + cards[cardId].rank);
+  console.log("Card Image " + cards[cardId].cardImage);
+  console.log("Card Suit " + cards[cardId].suit);
   checkForMatch();
 };
 
