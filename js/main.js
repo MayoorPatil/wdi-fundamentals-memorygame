130c60fc0015a258dfd2c1fbb0e4fdc0f2e11605
Patil <n0225550@libp45p-35426dd.lm.lmig.com>
2017-08-24 06:20:11 -0400

    updated game logic - unit 11

--- js/main.js
+++ js/main.js
@@ -21,24 +21,44 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
+var score = 0;
 
-var checkForMatch = function () {
+var checkForMatch = function (cardElement) {
   if (cardsInPlay.length === 2) {
     if ( cardsInPlay[0] === cardsInPlay[1]) {
       alert("You have found a match!");
+      score = score + 1;
+      document.getElementById('score').innerHTML = "Score: " + score;
     } else {
       alert("Sorry, try again.");
     }
   }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute("data-id");
   cardsInPlay.push(cards[cardId].rank);
-  console.log("User filpped " + cards[cardId].rank);
-  console.log("Card Image " + cards[cardId].cardImage);
-  console.log("Card Suit " + cards[cardId].suit);
-  checkForMatch();
+  this.setAttribute("src", cards[this.getAttribute("data-id")].cardImage);
+  checkForMatch(this);
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+var resetBoard = function() {
+  var cards = document.getElementsByTagName("img");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].setAttribute("src", "images/back.png");
+  }
+  cardsInPlay = [];
+};
+document.getElementsByTagName("button")[0].addEventListener("click", resetBoard);
+
+createBoard();
